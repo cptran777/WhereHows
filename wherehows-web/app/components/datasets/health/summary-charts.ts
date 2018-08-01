14846d91685cba0f6cef0feda70741dfd25d6c39
cptran777 <catran@linkedin.com>
2018-07-31 17:21:57 -0700

    Create a summary health metrics container component and render mock data for graphs

--- wherehows-web/app/components/datasets/health/summary-charts.ts
+++ wherehows-web/app/components/datasets/health/summary-charts.ts
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
 
-export default class DatasetsHealthSummaryCharts extends Component.extend({
-  // anything which *must* be merged to prototype here
-}) {
-  // normal class body definition here
+export default class DatasetsHealthSummaryCharts extends Component {
+  /**
+   * Sets the classes for the rendered html element for the component
+   * @type {Array<string>}
+   */
+  classNames = ['dataset-health__summary-charts'];
 }
